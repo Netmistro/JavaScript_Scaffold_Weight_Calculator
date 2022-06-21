bf15c66d2460219978637b05bd498c56400d4f68
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-06-21 17:11:01 -0400

    Remove all child elements from the main Div so they dont appear stacked on top of each other from previous selections

--- app.js
+++ app.js
@@ -2,63 +2,63 @@
 const scaffoldTubes = [
   {
     name: "Scaffold Steel Tube (4.0mm) - 21'",
-    weight: "28.98",
-    group: "Scaffold Tube",
+    weight: '28.98',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 20'",
-    weight: "27.60",
-    group: "Scaffold Tube",
+    weight: '27.60',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 18'",
-    weight: "24.84",
-    group: "Scaffold Tube",
+    weight: '24.84',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 16'",
-    weight: "22.08",
-    group: "Scaffold Tube",
+    weight: '22.08',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 14'",
-    weight: "19.32",
-    group: "Scaffold Tube",
+    weight: '19.32',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 12'",
-    weight: "16.56",
-    group: "Scaffold Tube",
+    weight: '16.56',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 10'",
-    weight: "13.80",
-    group: "Scaffold Tube",
+    weight: '13.80',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 08'",
-    weight: "11.04",
-    group: "Scaffold Tube",
+    weight: '11.04',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 06'",
-    weight: "8.28",
-    group: "Scaffold Tube",
+    weight: '8.28',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 04'",
-    weight: "5.52",
-    group: "Scaffold Tube",
+    weight: '5.52',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 02'",
-    weight: "2.76",
-    group: "Scaffold Tube",
+    weight: '2.76',
+    group: 'Scaffold Tube',
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 01'",
-    weight: "1.38",
-    group: "Scaffold Tube",
+    weight: '1.38',
+    group: 'Scaffold Tube',
   },
 ];
 
@@ -66,38 +66,38 @@ const scaffoldTubes = [
 const scaffoldBoard1_2m = [
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 13'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 10'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 08'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 06'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 04'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 02'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 01'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
 ];
 
@@ -105,72 +105,78 @@ const scaffoldBoard1_2m = [
 const scaffoldBoard1_5m = [
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 13'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 10'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 08'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 06'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 04'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 02'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 01'",
-    weight: "0",
-    group: "Scaffold Boards",
+    weight: '0',
+    group: 'Scaffold Boards',
   },
 ];
 
 // Add constants here
-const selectMaterialCategory = document.getElementById("material-dropdown");
-const materialListing = document.getElementById("list-container");
-const materialItem = document.querySelector(".matItem");
+const selectMaterialCategory = document.getElementById('material-dropdown');
+const materialListing = document.getElementById('list-container');
+const materialItem = document.querySelector('.matItem');
 
 // Onload hide the material container
 window.onload = (e) => {
-  materialListing.style.visibility = "hidden";
+  materialListing.style.visibility = 'hidden';
 };
 
 // On change or select of material category
-selectMaterialCategory.addEventListener("change", (e) => {
+selectMaterialCategory.addEventListener('change', (e) => {
   // Change the visibility of the container
-  materialListing.style.visibility = "visible";
+  materialListing.style.visibility = 'visible';
+
+  // Remove all child nodes if they exist from the parent container
+  while (materialListing.firstChild) {
+    materialListing.removeChild(materialListing.lastChild);
+  }
   // Switch the case depending on the selected and display the group
+
   switch (e.target.value) {
-    case "0":
+    case '0':
       scaffoldTubes.forEach((element) => {
         addNewButton();
         addNewInputQty();
         addNewSpanDescription(element.name);
       });
       break;
-    case "1":
+    case '1':
       scaffoldBoard1_2m.forEach((element) => {
         addNewButton();
         addNewInputQty();
         addNewSpanDescription(element.name);
       });
       break;
-    case "2":
+    case '2':
       scaffoldBoard1_5m.forEach((element) => {
         addNewButton();
         addNewInputQty();
@@ -184,35 +190,30 @@ selectMaterialCategory.addEventListener("change", (e) => {
 
 // Add New Add Button
 function addNewButton() {
-  const newAddButton = document.createElement("button");
-  newAddButton.classList.add("btn", "matItem", "mb-1", "btn-danger", "col-2");
-  newAddButton.innerText = "Add";
+  const newAddButton = document.createElement('button');
+  newAddButton.classList.add('btn', 'matItem', 'mb-1', 'btn-danger', 'col-2');
+  newAddButton.innerText = 'Add';
   materialListing.append(newAddButton);
 }
 
 // Add New Input Box
 function addNewInputQty() {
-  const newInput = document.createElement("input");
-  newInput.classList.add("matItem", "col-2", "mb-1");
-  newInput.type = "number";
-  newInput.placeholder = "Qty";
+  const newInput = document.createElement('input');
+  newInput.classList.add('matItem', 'col-2', 'mb-1');
+  newInput.type = 'number';
+  newInput.placeholder = 'Qty';
   materialListing.append(newInput);
 }
 
 // Add new Material Description using Span
 function addNewSpanDescription(materialDescription) {
-  const newMaterialDescription = document.createElement("span");
+  const newMaterialDescription = document.createElement('span');
   newMaterialDescription.classList.add(
-    "matItem",
-    "description",
-    "mb-1",
-    "col-8"
+    'matItem',
+    'description',
+    'mb-1',
+    'col-8'
   );
   newMaterialDescription.innerText = materialDescription;
   materialListing.append(newMaterialDescription);
-}
-
-// Remove all items within the Items DIV
-function removeMaterialItems() {
-  materialListing.remove;
 }
