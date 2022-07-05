3e315b351c79a37d3b3175f2061a9b5ad1ad7d29
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2022-07-05 15:59:23 -0400

    Update

--- app.js
+++ app.js
@@ -2,63 +2,63 @@
 const scaffoldTubes = [
   {
     name: "Scaffold Steel Tube (4.0mm) - 21'",
-    weight: '28.98',
-    group: 'Scaffold Tube',
+    weight: "28.98",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 20'",
-    weight: '27.60',
-    group: 'Scaffold Tube',
+    weight: "27.60",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 18'",
-    weight: '24.84',
-    group: 'Scaffold Tube',
+    weight: "24.84",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 16'",
-    weight: '22.08',
-    group: 'Scaffold Tube',
+    weight: "22.08",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 14'",
-    weight: '19.32',
-    group: 'Scaffold Tube',
+    weight: "19.32",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 12'",
-    weight: '16.56',
-    group: 'Scaffold Tube',
+    weight: "16.56",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 10'",
-    weight: '13.80',
-    group: 'Scaffold Tube',
+    weight: "13.80",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 08'",
-    weight: '11.04',
-    group: 'Scaffold Tube',
+    weight: "11.04",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 06'",
-    weight: '8.28',
-    group: 'Scaffold Tube',
+    weight: "8.28",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 04'",
-    weight: '5.52',
-    group: 'Scaffold Tube',
+    weight: "5.52",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 02'",
-    weight: '2.76',
-    group: 'Scaffold Tube',
+    weight: "2.76",
+    group: "Scaffold Tube",
   },
   {
     name: "Scaffold Steel Tube (4.0mm) - 01'",
-    weight: '1.38',
-    group: 'Scaffold Tube',
+    weight: "1.38",
+    group: "Scaffold Tube",
   },
 ];
 
@@ -66,38 +66,38 @@ const scaffoldTubes = [
 const scaffoldBoard1_2m = [
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 13'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 10'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 08'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 06'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 04'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 02'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.2m Support MG - 01'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
 ];
 
@@ -105,56 +105,56 @@ const scaffoldBoard1_2m = [
 const scaffoldBoard1_5m = [
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 13'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 10'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 08'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 06'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 04'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 02'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
   {
     name: "Scaffold Board BS EN 2482 1.5m Support MG - 01'",
-    weight: '0',
-    group: 'Scaffold Boards',
+    weight: "0",
+    group: "Scaffold Boards",
   },
 ];
 
 // Add constants here
-const selectMaterialCategory = document.getElementById('material-dropdown');
-const listContainer = document.getElementById('list-container');
-const outputContainer = document.querySelector('.output-container');
-const selectedMaterials = document.querySelectorAll('.selected-material')[0];
+const selectMaterialCategory = document.getElementById("material-dropdown");
+const listContainer = document.getElementById("list-container");
+const outputContainer = document.querySelector(".output-container");
+const selectedMaterials = document.querySelectorAll(".selected-material")[0];
 
 // Onload hide the material container
 window.onload = (e) => {
-  listContainer.style.visibility = 'hidden';
+  listContainer.style.visibility = "hidden";
 };
 
 // On change or select of material category
-selectMaterialCategory.addEventListener('change', (e) => {
+selectMaterialCategory.addEventListener("change", (e) => {
   // Change the visibility of the container
-  listContainer.style.visibility = 'visible';
+  listContainer.style.visibility = "visible";
 
   // Remove all child nodes if they exist from the parent container
   while (listContainer.firstChild) {
@@ -163,30 +163,36 @@ selectMaterialCategory.addEventListener('change', (e) => {
 
   // Switch the case depending on the selected and display the group
   switch (e.target.value) {
-    case '0':
+    case "0":
       count = 0;
       scaffoldTubes.forEach((element) => {
         addNewInputQty(count);
         addNewSpanDescription(element.name);
-        addNewUnitWeight(element.weight + '\n');
+        addNewUnitWeight(element.weight + "\n");
         count++;
       });
 
       //Add Button Query Selector
-      let addItemsButton = document.querySelector('.btn-add');
+      let addItemsButton = document.querySelector(".btn-add");
 
       // Constants for each element
-      let inputQtyAdd = document.querySelectorAll('.qty-add');
-      let descriptionAdd = document.querySelectorAll('.description-add');
-      let unitWeightAdd = document.querySelectorAll('.unit-weight-add');
+      let inputQtyAdd = document.querySelectorAll(".qty-add");
+      let descriptionAdd = document.querySelectorAll(".description-add");
+      let unitWeightAdd = document.querySelectorAll(".unit-weight-add");
 
       // Event listener for the click button
-      addItemsButton.addEventListener('click', (e) => {
+      addItemsButton.addEventListener("click", (e) => {
         // Loop through and add each element+
         for (let i = 0; i < inputQtyAdd.length; i++) {
-          if (!(inputQtyAdd[i].value === '' || inputQtyAdd[i].value === '0')) {
+          if (!(inputQtyAdd[i].value === "" || inputQtyAdd[i].value === "0")) {
             // Send all values to the console once the input field isn't empty
+            const outputDiv = document.createElement("div");
+            outputDiv.classList.add("out-list-item-" + i);
+            selectedMaterials.append(outputDiv);
 
+            let mat = document.querySelectorAll(".out-list-item-" + i);
+
+            console.log(mat);
             // Output Quantity
             qtyOutput(inputQtyAdd[i].value);
             // Out Description
@@ -199,12 +205,12 @@ selectMaterialCategory.addEventListener('change', (e) => {
         }
         // Clear all input boxes for quantities
         for (let j = 0; j < inputQtyAdd.length; j++) {
-          inputQtyAdd[j].value = '';
+          inputQtyAdd[j].value = "";
         }
       });
 
       break;
-    case '1':
+    case "1":
       scaffoldBoard1_2m.forEach((element) => {});
       break;
     default:
@@ -212,83 +218,80 @@ selectMaterialCategory.addEventListener('change', (e) => {
   }
 });
 
-// Input Screen
-// Add New Input Box
+// CODE FOR INPUT MATERIALS ABOVE
+// Input Quantity to the Input Screen Below
 function addNewInputQty(id) {
-  const newInput = document.createElement('input');
-  newInput.classList.add('qty-add');
-  newInput.type = 'number';
-  newInput.placeholder = 'Qty';
-  newInput.setAttribute('id', id);
+  const newInput = document.createElement("input");
+  newInput.classList.add("qty-add");
+  newInput.type = "number";
+  newInput.placeholder = "Qty";
+  newInput.setAttribute("id", id);
   listContainer.append(newInput);
 }
 
 // Add new Material Description using Span
 function addNewSpanDescription(materialDescription) {
-  const newMaterialDescription = document.createElement('span');
-  newMaterialDescription.classList.add('description-add');
+  const newMaterialDescription = document.createElement("span");
+  newMaterialDescription.classList.add("description-add");
   newMaterialDescription.innerText = materialDescription;
   listContainer.append(newMaterialDescription);
 }
 
 // Add new Weight using Span
 function addNewUnitWeight(weight) {
-  const unitWeight = document.createElement('span');
-  unitWeight.classList.add('unit-weight-add');
+  const unitWeight = document.createElement("span");
+  unitWeight.classList.add("unit-weight-add");
   unitWeight.innerText = weight;
   listContainer.append(unitWeight);
 }
 
 // Add new Total Weight using Span
 function addNewTotalWeight(totalWeightAdd) {
-  const totalWeight = document.createElement('span');
-  totalWeight.classList.add('unit-weight-add');
+  const totalWeight = document.createElement("span");
+  totalWeight.classList.add("unit-weight-add");
   totalWeight.innerText = totalWeightAdd;
   listContainer.append(totalWeight);
 }
 
 // Add new Span element for output container
 function addNewSpanElement(qty) {
-  const qtySpan = document.createElement('span');
-  qtySpan.classList.add('qty-span');
+  const qtySpan = document.createElement("span");
+  qtySpan.classList.add("qty-span");
   qtySpan.innerText = qty;
   outputContainer.append(qtySpan);
 }
 
+// CODE FOR OUTPUT MATERIALS BELOW
 // Quantity Output to the Output Screen Below
 function qtyOutput(quantity) {
-  const qtyOutputSpan = document.createElement('span');
-  qtyOutputSpan.classList.add('qty-out');
+  const qtyOutputSpan = document.createElement("span");
+  // qtyOutputSpan.classList.add("qty-out");
   qtyOutputSpan.innerText = quantity;
   selectedMaterials.append(qtyOutputSpan);
 }
 
 // Description output Span
 function outDescription(outDescription) {
-  const descriptionOutputSpan = document.createElement('span');
-  descriptionOutputSpan.classList.add('description-out');
+  const descriptionOutputSpan = document.createElement("span");
+  // descriptionOutputSpan.classList.add("description-out");
   descriptionOutputSpan.innerText = outDescription;
   selectedMaterials.append(descriptionOutputSpan);
+  selectedMaterials.append("\t");
 }
 
 // Unit Weight output Span - done send output to screen
 function outputUnitWeight(outUnitWeight) {
-  const unitWeightOutputSpan = document.createElement('span');
-  unitWeightOutputSpan.classList.add('unit-weight-out');
+  const unitWeightOutputSpan = document.createElement("span");
+  // unitWeightOutputSpan.classList.add("unit-weight-out");
   // selectedMaterials.append(unitWeightOutputSpan);
   // unitWeightOutputSpan.append(outUnitWeight);
 }
 
 // Output Span Total Weight
 function outTotalWeight(outQuantity, outUnitWeight) {
-  const totalWeightOutputSpan = document.createElement('span');
-  totalWeightOutputSpan.classList.add('total-weight-out');
-  totalWeightOutputSpan.innerText = outQuantity * outUnitWeight;
+  const totalWeightOutputSpan = document.createElement("span");
+  // totalWeightOutputSpan.classList.add("total-weight-out");
+  totalWeightOutputSpan.innerText = (outQuantity * outUnitWeight).toFixed(2);
   selectedMaterials.append(totalWeightOutputSpan);
-  selectedMaterials.append(document.createElement('br'));
-}
-
-// Add a Line Break
-function addLineBreak() {
-  return '<br>';
+  selectedMaterials.append(document.createElement("br"));
 }
